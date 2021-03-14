180a7a7f1fde64c25bb984897e0e363ab541dcd5
Djones A. Boni <djboni@gmail.com>
2021-03-13 23:43:16 -0300

    Improve declaration detection

--- load_c.py
+++ load_c.py
@@ -13,38 +13,6 @@ Remove_Unknowns = """\
 #define __restrict
 """
 
-def preprocess(source, include_paths, compiler_options):
-  command = ['gcc', *compiler_options, *include_paths, '-E', '-P', '-']
-  return subprocess.check_output(command, input=source, universal_newlines=True)
-
-class HeaderGenerator(pycparser.c_generator.CGenerator):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.functions = set()
-  def set_SourceContent(self, source_content):
-    self.source_content = source_content
-  def visit_Decl(self, n, *args, **kwargs):
-    import os
-    result = super().visit_Decl(n, *args, **kwargs)
-    if isinstance(n.type, pycparser.c_ast.FuncDecl):
-      # Is a function declaration
-      if n.name in self.functions:
-        # Is already in functions
-        return result
-      elif re.search(re.escape(result).replace('\\*', '\\*\\s*').replace('\\ ', '\\s*'), self.source_content) != None:
-        # Is declared in source content
-        return result
-      else:
-        # Not in functions, not in source
-        self.functions.add(n.name)
-        return 'extern "Python+C" ' + result
-    else:
-      # Not a function declaration
-      return result
-  def visit_FuncDef(self, n, *args, **kwargs):
-    self.functions.add(n.decl.name)
-    return ''
-
 def load(source_files, include_paths=[], compiler_options=[], remove_unknowns='', module_name='pysim_', avoid_cache=False):
   """Load a C file into Python as a module.
 
@@ -63,40 +31,34 @@ avoid_cache=True: makes random names to allow testing code with global variables
   # Create a list if just one souce file in a string
   if type(source_files) == str:
     source_files = [source_files,]
-  
+
   # Prepend -I on include paths
   include_paths = [ '-I' + x for x in include_paths ]
-  
+
   # Collect source code
   source_content = []
   for file in source_files:
     with open(file) as fp:
       source_content.append(fp.read())
   source_content = '\n'.join(source_content)
-  
+
   # Collect include files
   # TODO Remove inclusions inside comments
   header_content = ''.join(re.findall('\s*#include\s+.*', source_content))
-  
-  # Remove unknowns to CFFI
-  header_content = Remove_Unknowns + remove_unknowns + header_content
-  
+
   # Preprocess include files
+  header_content = Remove_Unknowns + remove_unknowns + header_content
   header_content = preprocess(header_content, include_paths, compiler_options)
-  
-  # Prepend 'extern "Python+C" ' to functions declarations with no definitions 
-  ast_header = pycparser.CParser().parse(header_content)
-  header_generator = HeaderGenerator()
-  header_generator.set_SourceContent(source_content)
-  header_content = header_generator.visit(ast_header)
-  
+
   # Preprocess source code
   source_content = Remove_Unknowns + remove_unknowns + source_content
   source_content = preprocess(source_content, include_paths, compiler_options)
-  
+
   # Remove conflicts
+  #header_content = header_content.replace('typedef struct { int __val[2]; } __fsid_t;', '')
   source_content = source_content.replace('typedef struct { int __val[2]; } __fsid_t;', '')
-  # Rename main, to avoid any conflicts, and declare it
+
+  # Rename main, to avoid any conflicts, and declare it in the header
   if 'int main(' in source_content:
     # TODO REGEX
     source_content = source_content.replace('int main(', 'int mpmain(')
@@ -105,13 +67,19 @@ avoid_cache=True: makes random names to allow testing code with global variables
     # TODO REGEX
     source_content = source_content.replace('void main(', 'void mpmain(')
     header_content += '\nvoid mpmain(void argc, char **argv);\n'
-  
+
+  # Prepend 'extern "Python+C" ' to functions declarations with no definitions 
+  ast_header = pycparser.CParser().parse(header_content)
+  header_generator = HeaderGenerator()
+  header_generator.set_SourceContent(source_content)
+  header_content = header_generator.visit(ast_header)
+
   # Run CFFI
   ffibuilder = cffi.FFI()
   ffibuilder.cdef(header_content)
-  include_dirs = [x.replace('-I', '') for x in include_paths]
+  include_dirs = [ x.replace('-I', '') for x in include_paths ]
   if 0:
-    # Did not work
+    # export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.4
     extra_compile_args = ['-fsanitize=address']
     libraries = ['asan']
   else:
@@ -119,10 +87,47 @@ avoid_cache=True: makes random names to allow testing code with global variables
     libraries = []
   ffibuilder.set_source(module_name, source_content, include_dirs=include_dirs, extra_compile_args=extra_compile_args, libraries=libraries)
   ffibuilder.compile()
-  
+
   # Import and return resulting module
   module = importlib.import_module(module_name)
 
   # Return both the library object and the ffi object
   return module.lib, module.ffi
+
+def preprocess(source, include_paths, compiler_options):
+  command = ['gcc', *compiler_options, *include_paths, '-E', '-P', '-']
+  return subprocess.check_output(command, input=source, universal_newlines=True)
+
+class HeaderGenerator(pycparser.c_generator.CGenerator):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.functions = set()
+  def set_SourceContent(self, source_content):
+    self.source_content = source_content
+  def visit_Decl(self, n, *args, **kwargs):
+    import os
+    result = super().visit_Decl(n, *args, **kwargs)
+    if isinstance(n.type, pycparser.c_ast.FuncDecl):
+      # Is a function declaration
+      if n.name in self.functions:
+        # Is already in functions
+        return result
+      elif re.search((
+                      re.escape(result)
+                          .replace('\\*', '\\*\\s*')
+                          .replace('\\ ', '\\s*')
+                          + '\\s*\{'
+                     ), self.source_content) != None:
+        # Is declared in source content
+        return result
+      else:
+        # Not in functions, not in source
+        self.functions.add(n.name)
+        return 'extern "Python+C" ' + result
+    else:
+      # Not a function declaration
+      return result
+  def visit_FuncDef(self, n, *args, **kwargs):
+    self.functions.add(n.decl.name)
+    return ''
 
